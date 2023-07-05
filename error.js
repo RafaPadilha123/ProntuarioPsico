a4e59433b91ad797986376d8a8c341883c01e390
Rafael Rodrigues Padilha <116320412+RafaPadilha123@users.noreply.github.com>
2023-07-05 15:54:56 -0300

    Add files via upload

---
+++ error.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+
+$(document).ready(() => {
+  $('.logo').slideDown(2000);
+});
